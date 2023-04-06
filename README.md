8b988bbd58e1d5658a0b0a4f31246faba93cc109
mnj141 <123129651+mnj141@users.noreply.github.com>
2023-04-05 22:24:39 -0500

    updated

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # s23wb45rama
 Hosted at : https://s23wb45rama.onrender.com/
+
+Hi, I am Manoj  Rama
+class: electronics
+Attributes: electronicsName(string), electronicsType(string), schoolCost(number)
--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var electronicsRouter = require('./routes/electronics');
+var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -20,7 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/electronics', electronicsRouter);
+app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
